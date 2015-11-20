baef0170f069da6d835bc4aec594da91bcd6cca4
Ailtonjr <ailtoncardoso0@gmail.com>
2015-11-20 15:45:47 -0200

    Adicionado: 	- SQL

---
+++ DelGIS/DelGIS_bd.sql
@@ -0,0 +1,41 @@
+
+CREATE EXTENSION postgis;
+
+CREATE TABLE bairro(
+	id SERIAL,
+	nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tipo_ocorrencia (
+	id SERIAL,
+	nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE ocorrencia (
+	id SERIAL,
+	descricao VARCHAR(255) NOT NULL,
+	id_tipo INTEGER
+);
+
+ALTER TABLE bairro ADD PRIMARY KEY(id);
+ALTER TABLE tipo_ocorrencia ADD PRIMARY KEY(id);
+ALTER TABLE ocorrencia ADD PRIMARY KEY(id);
+
+ALTER TABLE ocorrencia ADD FOREIGN KEY (id_tipo) REFERENCES tipo_ocorrencia(id) ON DELETE CASCADE;
+
+-- SIRGAS 2000 datum 4674
+SELECT AddGeometryColumn ('public', 'bairro', 'coordenadas', 4674, 'POLYGON', 2);
+SELECT AddGeometryColumn ('public', 'ocorrencia', 'coordenada', 4674, 'POINT', 2);
+
+-- Quatro pontos pois o quarto e o primeiro
+INSERT INTO bairro(nome, coordenadas) VALUES('abacate', st_geomfromtext(
+	'POLYGON((6 15, 10 10, 20 10, 6 15))', 4674) );
+
+-- Inserir tipo
+INSERT INTO tipo_ocorrencia(id,nome) VALUES (0,'Roubo');
+	
+-- Inserir ponto
+INSERT INTO ocorrencia (descricao, coordenada, id_tipo) VALUES ('Local 1', st_geomfromtext('POINT(10 20)', 4674), 0);
+
+-- Traz a cordenada transformada
+SELECT st_astext(coordenadas) FROM bairro;
