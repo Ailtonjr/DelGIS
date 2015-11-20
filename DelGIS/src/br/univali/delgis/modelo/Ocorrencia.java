cc3ec2e14608125f285a4947143fd1e4e8d69ed2
Ailtonjr <ailtoncardoso0@gmail.com>
2015-11-20 15:22:16 -0200

    Adicionado: 	- Atributos da classe Ocorrencia

--- DelGIS/src/br/univali/delgis/modelo/Ocorrencia.java
+++ DelGIS/src/br/univali/delgis/modelo/Ocorrencia.java
@@ -10,5 +10,40 @@ package br.univali.delgis.modelo;
  * @author 5122929
  */
 public class Ocorrencia {
-    
+    private String descricao;
+    private double longitude;
+    private double latitude;
+    private String tipoOcorrencia;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getTipoOcorrencia() {
+        return tipoOcorrencia;
+    }
+
+    public void setTipoOcorrencia(String tipoOcorrencia) {
+        this.tipoOcorrencia = tipoOcorrencia;
+    }
 }
