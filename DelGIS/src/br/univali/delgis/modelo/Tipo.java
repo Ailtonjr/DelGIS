9eeaab5c35134eb1ba6f31af15d090b1a60fff8c
João <Joo@192.168.25.73>
2015-11-23 16:13:38 -0200

    Delete adicionado

--- DelGIS/src/br/univali/delgis/modelo/Tipo.java
+++ DelGIS/src/br/univali/delgis/modelo/Tipo.java
@@ -2,6 +2,7 @@
 package br.univali.delgis.modelo;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
@@ -53,6 +54,36 @@ public class Tipo {
         }
     }
 
+    public void excluiBanco() {
+        String sql = "BEGIN;"
+                + "DELETE FROM tipo_ocorrencia WHERE id = ?;"
+                + "COMMIT";
+        
+        try {
+            PreparedStatement statement = con.getConexao().prepareStatement(sql);
+            statement.setString(1, id);
+            statement.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Tipo " + nome + " inserido com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao inserir tipo " + nome + ". Verifique se este tipo já não existente\n" + sql);
+            ex.printStackTrace();
+        }
+    }
+    
+    public ResultSet buscaTipos() {
+        String sql = "";
+        ResultSet rs = null;
+
+        try {
+            PreparedStatement statement = con.getConexao().prepareStatement(sql);
+            rs = statement.executeQuery(sql);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar chamados\n" + sql, "Erro", JOptionPane.ERROR_MESSAGE);
+            ex.printStackTrace();
+        }
+        return rs;
+    }
+    
     public String getId() {
         return id;
     }
